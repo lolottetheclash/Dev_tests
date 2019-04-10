fe8ddf7710f12352417f64318390af3b0be946a4
lauren <laurenzolivier@gmail.com>
2019-04-10 21:26:05 +0200

    add new folder and 1st exercise

---
+++ Algo/Algo_1.rb
@@ -0,0 +1,44 @@
+#Problème 1
+#Écrire 3 fonctions qui calculent la somme des nombres dans une liste utilisant 3 façons différentes (while, each, recursion en ruby)
+
+list = [1, 67, 34, 25, 9, 76, 3]
+
+def adder_while(list)
+	x = 0
+	l = list.length
+	sum = 0
+	while x < l
+		sum += list[x]
+		x += 1
+	end
+	puts sum
+end
+
+adder_while(list)
+
+
+def adder_each(list)
+	x = 0
+	l = list.length
+	sum = 0
+	list.each do |item|
+		sum += item
+		x += 1
+	end
+	puts sum
+end
+
+adder_each(list)
+
+
+def adder_recursive(list)
+  if list.empty?
+  	return 0
+  else
+  	n = list.pop
+  	return n + adder_recursive(list)
+  end
+end
+
+puts adder_recursive(list)
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Code Tests From https://github.com/florinpop17/app-ideas
+Beginner Projects : Code Tests From https://github.com/florinpop17/app-ideas
+
+Algo : Code Tests Fromhttps://gist.github.com/denispasin/45a534fe89e3b96eb68f7b65c3ef436b
